824793706ab84c3b856f96d2de359ef24ee0bc2c
Flavio Souza <flavioluiz.ssouza@gmail.com>
2022-09-01 18:56:32 -0300

    project artifacts

---
+++ src/IC_Arrays.c
@@ -0,0 +1,168 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <png.h>
+#include <gd.h>
+
+#define ITERATIONS 150000
+#define INIT_TEMP_HOT 100
+#define INIT_TEMP_NEUTRAL 0
+#define COLUMN 300
+#define ROW    300
+#define SIZE_IMAGE 10
+
+struct Whole_matrix {
+       double calculated[ROW][COLUMN]; 
+       double updated[ROW][COLUMN];      
+};
+
+enum TYPE_IMAGE {IMAGE_BEGIN, IMAGE_END};
+
+void outputMatrixToPNG(double matrix[][COLUMN],enum TYPE_IMAGE type) {
+	int maxvalue =100;
+	int i,j, tot = 0;
+    FILE *pngout;
+
+    gdImagePtr im = gdImageCreate(COLUMN, ROW);
+
+    gdImageColorAllocate(im, 255, 255, 255);
+
+    for(i = 0; i < COLUMN; i++) {
+        for (j =0; j < ROW; j++) {
+        	int valor = 100 - (int)(matrix[i][j]);
+            int color = gdImageColorExact(im, valor, valor, valor) ;
+            if (color == -1) { 
+                color = gdImageColorAllocate(im, valor, valor, valor);
+            }
+            gdImageSetPixel(im, j, i, color);
+        }
+    }
+
+    if (IMAGE_BEGIN == type)
+    	pngout = fopen("imageIcSequencial_Begin.png", "wb");
+    else if(IMAGE_END == type)
+    	pngout = fopen("imageIcSequencial_End.png", "wb");
+
+    gdImagePng(im, pngout);
+
+    fclose(pngout);
+
+    gdImageDestroy(im);
+   
+    return;
+}
+
+
+void mountsEmptyArray(double array[][COLUMN]){
+     int i, j;
+     /* The initial temperature of the first row for the first 
+        column takes the value of 'INIT_TEMP_HOT' */ 
+     for (j = 0; j < COLUMN; j++)    
+         array[0][j] = INIT_TEMP_HOT;   
+      
+     /* The other cells receive zero values */ 
+     for (i = 1; i < ROW; i++)  { 
+    	 for (j = 0; j < COLUMN; j++) 
+             array[i][j] = INIT_TEMP_NEUTRAL;     
+     }       
+}
+
+
+void printArray(double array[][COLUMN]){
+     int i, j;
+     printf("\n \n");
+     for (i = 0; i < ROW; i++)  {
+         for (j = 0; j < COLUMN; j++) 
+             printf("| %f ",array[i][j]);     
+         printf("\n");    
+     }                
+}
+
+
+/* The parameter 'i' represented the horizontal coordinate (row) 
+   and the parameter 'j' the vertical coordinate (column) */ 
+double calculatesNewTemperature(double array[][COLUMN], int i, int j){
+      int computes_dividers = 1;
+      double accumulates_current_temp = array[i][j];
+ 
+      // computes values of cells available in row             
+      if(i == 0){
+             accumulates_current_temp += array[i+1][j];  
+             computes_dividers += 1;          
+      }else if(i == ROW-1){
+             accumulates_current_temp += array[i-1][j];
+             computes_dividers += 1;           
+      }else if(i > 0 && i < ROW-1){
+             accumulates_current_temp += array[i-1][j];
+             accumulates_current_temp += array[i+1][j];
+             computes_dividers += 2;   
+      }
+     
+      // computes values of cells available in column                      
+      if(j == 0){
+             accumulates_current_temp += array[i][j+1];
+             computes_dividers += 1;
+      }else if(j == COLUMN-1){
+            accumulates_current_temp += array[i][j-1];            
+            computes_dividers += 1;
+      }else if(j > 0 && j < COLUMN-1){
+            accumulates_current_temp += array[i][j-1];            
+            accumulates_current_temp += array[i][j+1];            
+            computes_dividers += 2;
+      } 
+       
+      return (accumulates_current_temp / computes_dividers);         
+}
+
+
+void calculateHeatTransfer(double updated[][COLUMN], double calculated[][COLUMN]){
+  int i,j;
+  for(i = 1; i < ROW; i++){
+      for(j = 0; j < COLUMN; j++)
+    	  updated[i][j] = calculatesNewTemperature(calculated, i, j);
+  }
+  //printArray(updated);
+}
+
+int main(int argc, char **argv) {
+	time_t t1,t2;
+	(void) time(&t1);
+	  
+	struct Whole_matrix matrix;
+	mountsEmptyArray(matrix.calculated);
+	mountsEmptyArray(matrix.updated);
+  
+	if (ITERATIONS%2 == 0){
+		outputMatrixToPNG(matrix.calculated,IMAGE_BEGIN);
+//			printArray(matrix.calculated);
+	}
+	else{
+		outputMatrixToPNG(matrix.updated,IMAGE_BEGIN);
+//			printArray(matrix.updated);
+	}
+	
+	int cont = 0;
+	while(cont <= ITERATIONS){
+		if(cont%2 == 0)
+			calculateHeatTransfer(matrix.updated, matrix.calculated);
+		else
+			calculateHeatTransfer(matrix.calculated, matrix.updated);
+		cont++;
+	}  
+	
+	(void) time(&t2);
+	printf("\nO tempo de processamento foi de %f segundos.", (double) t2-t1);
+	  
+	if (ITERATIONS%2 == 0){
+		outputMatrixToPNG(matrix.calculated,IMAGE_END);
+//			printArray(matrix.calculated);
+	}
+	else{
+		outputMatrixToPNG(matrix.updated,IMAGE_END);
+//			printArray(matrix.updated);
+	}
+	return 0;
+}
+
+
